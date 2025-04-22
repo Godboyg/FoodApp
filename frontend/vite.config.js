d182c539a7be2f2fb1fe57ac0f7785620dec8277
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-22 20:11:59 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
       '/api': {
         target: 'https://foodapp-733a.onrender.com',
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
