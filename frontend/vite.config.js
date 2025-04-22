bd753222ae4e6173bed33d0567008f3741792b9b
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-22 21:13:05 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,9 @@
-// import { defineConfig } from 'vite'
+import { defineConfig } from 'vite' 
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default({
-  server: {
-    proxy: {
-      '/api': 'https://foodapp-733a.onrender.com'
-    },
-  },
+export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
