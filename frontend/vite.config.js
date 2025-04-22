f4a3ee2c024fbcfffdbc3b45edbb51e5c56706a3
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-22 20:47:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'https://foodapp-733a.onrender.com',
-        changeOrigin: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+      '/api': 'https://foodapp-733a.onrender.com'
     },
   },
   plugins: [
